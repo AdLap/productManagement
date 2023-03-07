715380767d02772960f910e52adcbbebac9ae62a
AdLap <a.lapacz@global4net.com>
2023-03-07 13:55:39 +0100

    Initial product card width

--- src/components/catalog/products/card/ProductCard.vue
+++ src/components/catalog/products/card/ProductCard.vue
@@ -1,6 +1,7 @@
 <template>
   <VContainer fluid class="d-flex justify-sm-center align-center h-90 ma-auto">
     <VCard
+      width="90%"
       max-width="800"
       min-height="60vh"
       class="product-card pa-4 d-flex flex-column"
